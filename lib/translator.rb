8937d474eef1d61f992e77361184df4b0a4d3c89
Alethia Quintero <alethiaq22@gmail.com>
2019-07-05 23:45:43 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,8 +1,24 @@
 # require modules here
+require 'yaml'
 
-def load_library
-  # code goes here
+def load_library(file)
+  
+  emoticons = YAML.load_file(file)
+  emoticons.each do |meaning, faces|
+    if !emoticons["get_meaning"]
+      emoticons["get_meaning"] = {}
+    end
+    if !emoticons["get_emoticon"] 
+      emoticons["get_emoticon"] = {}
+    end
+    emoticons["get_meaning"] += meaning
+    faces.each do 
+    
+    
+  return emoticons
+
 end
+
 
 def get_japanese_emoticon
   # code goes here
